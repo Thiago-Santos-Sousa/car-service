e3720d20a2be3c7ddaae03c6dbde9e001e8ac1e4
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-09-06 22:53:29 -0300

    Atualizando interface.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CardModule} from 'primeng/card';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CardModule,
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
