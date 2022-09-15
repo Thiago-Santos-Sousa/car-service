abe32ebe35b3b4f824d2eed59da512df4d281b95
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-09-15 16:51:55 -0300

    Atualização alert service.

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RequestLogin } from 'src/app/resources/models/RequestLogin';
+import { AlertService } from 'src/app/resources/services/alert.service';
 import { LoginService } from 'src/app/resources/services/login.service';
 
 
@@ -13,18 +14,18 @@ export class LoginComponent implements OnInit {
 
   public requestLogin!: RequestLogin;
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private alertService: AlertService) { }
 
   ngOnInit(): void {
     this.requestLogin = new RequestLogin();
   }
 
   public doLogin() :void{
-    this.loginService.doLogin(this.requestLogin).subscribe(data =>{
-      console.log(data);
+    this.loginService.doLogin(this.requestLogin).subscribe((data) =>{
+      this.alertService.info("Funcionalidade ainda não implementada!");
     }, 
-    error => {
-      console.error(error);
+    (httpError) => {
+      this.alertService.error(httpError.error.message);
     })
   }
 }
