b8e4dcba500faedd0e46666281741e339152d652
Thiago-Santos-Sousa <Santosthiagosampaio@gmail.com>
2022-09-15 17:22:24 -0300

    Implantando dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent}
+  { path: '', component: LoginComponent},
+
+  { path: 'dashboard',
+    loadChildren: ()=> 
+      import('./views/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
 ];
 
 @NgModule({
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { RequestLogin } from 'src/app/resources/models/RequestLogin';
 import { AlertService } from 'src/app/resources/services/alert.service';
 import { LoginService } from 'src/app/resources/services/login.service';
@@ -14,7 +15,12 @@ export class LoginComponent implements OnInit {
 
   public requestLogin!: RequestLogin;
 
-  constructor(private loginService: LoginService, private alertService: AlertService) { }
+  constructor(
+    private loginService: LoginService, 
+    private alertService: AlertService,
+    private router: Router
+  )
+  { }
 
   ngOnInit(): void {
     this.requestLogin = new RequestLogin();
@@ -22,7 +28,7 @@ export class LoginComponent implements OnInit {
 
   public doLogin() :void{
     this.loginService.doLogin(this.requestLogin).subscribe((data) =>{
-      this.alertService.info("Funcionalidade ainda não implementada!");
+      this.router.navigate(['dashboard']);
     }, 
     (httpError) => {
       this.alertService.error(httpError.error.message);
